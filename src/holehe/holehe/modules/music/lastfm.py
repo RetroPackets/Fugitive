4df98b4ceeca2687bb50811067b4e425131e3586
RetroPackets <103104601+RetroPackets@users.noreply.github.com>
2022-12-11 03:25:56 -0500

    Add files via upload

---
+++ src/holehe/holehe/modules/music/lastfm.py
@@ -0,0 +1,55 @@
+from holehe.core import *
+from holehe.localuseragent import *
+
+
+async def lastfm(email, client, out):
+    name = "lastfm"
+    domain = "last.fm"
+    method= "register"
+    frequent_rate_limit=False
+
+    try:
+        req = await client.get("https://www.last.fm/join")
+        token = req.cookies["csrftoken"]
+    except Exception:
+        out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                    "rateLimit": True,
+                    "exists": False,
+                    "emailrecovery": None,
+                    "phoneNumber": None,
+                    "others": None})
+        return None
+
+    data = {"csrfmiddlewaretoken": token, "userName": "", "email": email}
+    headers = {
+        "Accept": "*/*",
+        "Referer": "https://www.last.fm/join",
+        "X-Requested-With": "XMLHttpRequest",
+        "Cookie": "csrftoken=" + str(token),
+    }
+    try:
+
+        check = await client.post("https://www.last.fm/join/partial/validate", headers=headers, data=data)
+    except Exception:
+        out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                    "rateLimit": True,
+                    "exists": False,
+                    "emailrecovery": None,
+                    "phoneNumber": None,
+                    "others": None})
+        return None
+
+    if check.json()["email"]["valid"]:
+        out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                    "rateLimit": False,
+                    "exists": False,
+                    "emailrecovery": None,
+                    "phoneNumber": None,
+                    "others": None})
+    else:
+        out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                    "rateLimit": False,
+                    "exists": True,
+                    "emailrecovery": None,
+                    "phoneNumber": None,
+                    "others": None})
